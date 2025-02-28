2a448adea423d5faf1eac26726e04cc372ecd94c
Sergey-Bjj <sergey683hp@gmail.com>
2025-02-28 13:53:46 +0300

    Delete CarStore.Core/Abstractions/ITokenService.cs

--- CarStore.Core/Abstractions/ITokenService.cs
+++
@@ -1,8 +0,0 @@
-﻿using CarsStore.Core.Models;
-
-public interface ITokenService
-{
-    Task<(string AccessToken, string RefreshToken)> GenerateTokensAsync(User user);
-    Task<(string AccessToken, string RefreshToken)?> RefreshTokenAsync(string refreshToken);
-    Task RevokeRefreshTokenAsync(string refreshToken);
-}
